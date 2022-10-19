6ed286e4bac98e9533b7bed9c8b2387f80a505fc
Md. Abu Musa <musa.fci@gmail.com>
2022-10-19 23:13:19 +0600

    Project Setup & Required Package Install

---
+++ src/app.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {Container, Row, Col} from "reactstrap";
+import MainContent from "./components/main-content";
+import Sidebar from "./components/sidebar";
+
+import POLLS from "./data/polls";
+
+class App extends React.Component {
+    render() {
+        return (
+            <Container className="my-5">
+                <Row>                
+                    <Col md={4}>
+                        <Sidebar/>
+                    </Col>
+                    <Col md={8}>
+                        <MainContent/>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+}
+
+export default App;
---
+++ src/data/polls.js
@@ -0,0 +1,44 @@
+const polls = [
+    {
+        id:'342w22',
+        title:'What is your favourite programming language?',
+        description:'there are lot of popular programming languages available.Among them what is your favourite',
+        option:[
+            {id:'1a' , value:'C programming' ,vote:0},
+            {id:'2a' , value:'Java' ,vote:0},
+            {id:'3a' , value:'JavaScript' ,vote:0},
+            {id:'4a' , value:'Python' ,vote:0}
+        ],
+        created:new Date(),
+        totalVote:0,
+        opinions:[]
+    },
+    {
+        id:'dd334',
+        title:'What is your favourite programming language?',
+        description:'there are lot of popular programming languages available.Among them what is your favourite',
+        option:[
+            {id:'1a' , value:'C programming' ,vote:0},
+            {id:'2a' , value:'Java' ,vote:0},
+            {id:'3a' , value:'JavaScript' ,vote:0},
+            {id:'4a' , value:'Python' ,vote:0}
+        ],
+        created:new Date(),
+        totalVote:0,
+        opinions:[]
+    },
+    {
+        id:'CC749',
+        title:'What is your favourite programming language?',
+        description:'there are lot of popular programming languages available.Among them what is your favourite',
+        option:[
+            {id:'1a' , value:'C programming' ,vote:0},
+            {id:'2a' , value:'Java' ,vote:0},
+            {id:'3a' , value:'JavaScript' ,vote:0},
+            {id:'4a' , value:'Python' ,vote:0}
+        ],
+        created:new Date(),
+        totalVote:0,
+        opinions:[]
+    },
+];
