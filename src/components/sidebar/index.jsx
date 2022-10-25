341061990c0f3885d4486fb9a420a06893948b64
Md. Abu Musa <musa.fci@gmail.com>
2022-10-25 23:44:33 +0600

    Implement smart poll sidebar

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-
+import {Input, Button, Modal, ModalHeader, ModalBody} from "reactstrap";
+import PollList from "./poll_list";
 class Sidebar extends React.Component {
     render() {
         return(
-            <div>
-                <h1>Sidebar Component</h1>
+            <div style={{background: '#efefef', padding: '10px'}}>
+                <div className="d-flex mb-5">
+                    <Input type="search" placeholder="Search" onChange={e => this.props.handleSearch(e.target.value)} />
+                </div>
             </div>
         );
     }
